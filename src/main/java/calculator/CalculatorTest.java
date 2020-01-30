cacbb2ca3779232470b97702c062ba2e1fd4b251
chillgu <bbonggu92@gmail.com>
2020-01-30 10:45:46 +0900

    be delegated to other methods

--- src/main/java/calculator/CalculatorTest.java
+++ src/main/java/calculator/CalculatorTest.java
@@ -3,10 +3,26 @@ package calculator;
 public class CalculatorTest {
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
-		System.out.println(cal.add(3, 4));
-		System.out.println(cal.substract(5, 2));
-		System.out.println(cal.multiply(2, 3));
-		System.out.println(cal.divide(6, 2));
+		add(cal);
+		substract(cal);
+		multiply(cal);
+		divide(cal);
 		
+	}
+	
+	private static void add(Calculator cal) {
+		System.out.println(cal.add(4, 2));
+	}
+	
+	private static void substract(Calculator cal) {
+		System.out.println(cal.substract(4, 2));
+	}
+	
+	private static void multiply(Calculator cal) {
+		System.out.println(cal.multiply(4, 2));
+	}
+	
+	private static void divide(Calculator cal) {
+		System.out.println(cal.divide(4, 2));
 	}
 }
