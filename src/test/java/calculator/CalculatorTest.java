bfc7f6c3e57a0b99d23e7d78900d32523d3cab4a
chillgu <bbonggu92@gmail.com>
2020-01-30 11:34:50 +0900

    checked duplicates in test code

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,31 +2,45 @@ package calculator;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
-	
+	private Calculator cal;
+
+	@Before
+	public void setup() {
+		cal = new Calculator();
+		System.out.println("before");
+	}
 	@Test
 	public void add() {
-		Calculator cal = new Calculator();
 		assertEquals(3, cal.add(1, 2));
+		System.out.println("add");
 	}
 	
 	@Test
 	public void substract() {
-		Calculator cal = new Calculator();
 		assertEquals(3, cal.substract(5, 2));
+		System.out.println("substract");
 	}
 	
 	@Test
 	public void multiply() {
-		Calculator cal = new Calculator();
 		assertEquals(6, cal.multiply(2, 3));
+		System.out.println("multiply");
 	}
 	
 	@Test
 	public void divide() {
-		Calculator cal = new Calculator();
 		assertEquals(3, cal.divide(12, 4));
+		System.out.println("divide");
+	}
+	
+	@After
+	public void teardown() {
+		System.out.println("teardown");
 	}
 }
+
